0dd6f23e37d2a814d638268b88084ce6f4b6e299
Anshul Kumar <thehackerscore@gmail.com>
2021-12-28 15:43:04 +0530

    commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn egi.wsgi --log-file -
--- egi/settings.py
+++ egi/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-wdke-rd%j16gdotni4rj$mgdqy__%d4#sin44zug-z67e!(xg0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','egiportal.herokuapp.com']
 
 
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles",
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
