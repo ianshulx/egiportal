d4b710875ed30cd863ceb053b7cddf84ec9adb97
Anshul Kumar <thehackerscore@gmail.com>
2022-01-26 18:49:01 +0530

    Update admin.py

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import bca_sem1_notes, bca_sem2_notes,bca_sem3_notes,bca_sem4_notes,bca_sem5_notes,bca_sem6_notes,btech_sem1_notes,btech_sem2_notes, btech_cs_sem3_notes,btech_cs_sem4_notes,btech_cs_sem5_notes,btech_cs_sem6_notes,btech_cs_sem7_notes,btech_cs_sem8_notes
 from .models import btech_ce_sem3_notes,btech_ce_sem4_notes,btech_ce_sem5_notes,btech_ce_sem6_notes,btech_ce_sem7_notes,btech_ce_sem8_notes,btech_me_sem3_notes,btech_me_sem4_notes,btech_me_sem5_notes,btech_me_sem6_notes,btech_me_sem7_notes,btech_me_sem8_notes,btech_ee_sem3_notes,btech_ee_sem4_notes,btech_ee_sem5_notes,btech_ee_sem6_notes,btech_ee_sem7_notes,btech_ee_sem8_notes
+from .models import mba_sem1_notes, mba_sem2_notes, mba_sem3_notes, mba_sem4_notes
 # Register your models here.
 
 admin.site.register(bca_sem1_notes)
@@ -9,6 +10,7 @@ admin.site.register(bca_sem3_notes)
 admin.site.register(bca_sem4_notes)
 admin.site.register(bca_sem5_notes)
 admin.site.register(bca_sem6_notes)
+
 admin.site.register(btech_sem1_notes)
 admin.site.register(btech_sem2_notes)
 
@@ -44,3 +46,9 @@ admin.site.register(btech_ee_sem5_notes)
 admin.site.register(btech_ee_sem6_notes)
 admin.site.register(btech_ee_sem7_notes)
 admin.site.register(btech_ee_sem8_notes)
+
+
+admin.site.register(mba_sem1_notes)
+admin.site.register(mba_sem2_notes)
+admin.site.register(mba_sem3_notes)
+admin.site.register(mba_sem4_notes)
