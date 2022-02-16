3a147a4a61e58f2a6229e08a5a4c256d7b674b81
Anshul Kumar <thehackerscore@gmail.com>
2022-02-16 14:14:57 +0530

    c

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.forms import FileField, ImageField
 
-# Create your models here.
+class proof(models.Model):
+    full_name=models.CharField(max_length=100)
+    dob =models.DateField(auto_now=False, auto_now_add=False)
+    upload = ImageField( upload_to='static/proof', height_field=None, width_field=None, max_length=None)
+    proof_type=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.full_name
+
+    
+       
